94f5c8a8a2b0792323c657bb6d27a186c3be1ae3
Artem Slipets <someboygit@gmail.com>
2022-06-26 19:59:40 +0300

    Аннотация @Autowired Внедрение зависимостей (Dependency Injection).

--- src/main/java/ru/someboy/radar/MusicPlayer.java
+++ src/main/java/ru/someboy/radar/MusicPlayer.java
@@ -1,55 +1,22 @@
 package ru.someboy.radar;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MusicPlayer {
-    private Music music;
-    private List<Music> musicList = new ArrayList<>();
+    private final ClassicalMusic classicalMusic;
+    private final RockMusic rockMusic;
 
-    private String name;
-    private String volume;
-
-    public String getName() {
-        return name;
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String playMusic() {
+        return "Playing: " + classicalMusic.getSong();
+//        System.out.println("Playing: " + classicalMusic.getSong());
+//        System.out.println("Playing: " + rockMusic.getSong());
     }
-
-    public String getVolume() {
-        return volume;
-    }
-
-    public void setVolume(String volume) {
-        this.volume = volume;
-    }
-
-    public List<Music> getMusicList() {
-        return musicList;
-    }
-
-    public void setMusicList(List<Music> musicList) {
-        this.musicList = musicList;
-    }
-
-    // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public MusicPlayer() {}
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
-    }
-
-//    public void playMusicList() {
-//        musicList.forEach(song -> System.out.println("Playing: " + song.getSong()));
-//    }
 }
--- src/main/java/ru/someboy/radar/TestSpring.java
+++ src/main/java/ru/someboy/radar/TestSpring.java
@@ -8,10 +8,18 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-        musicPlayer.playMusic();
-//        System.out.println(music.getSong());
+//        Music music = context.getBean("rockMusic", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        musicPlayer.playMusic();
+//
+//        Music music2 = context.getBean("classicalMusic", Music.class);
+//        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
+//        classicalMusicPlayer.playMusic();
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
